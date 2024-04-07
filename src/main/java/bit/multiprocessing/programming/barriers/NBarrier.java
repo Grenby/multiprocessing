08a7de72ce3363437007522e64a33944e698250e
Grenby <dm.fedorov,cc@gmail.com>
2024-04-08 00:51:57 +0300

    ex 86 use AtomicIntegerArray and refactore

--- src/main/java/bit/multiprocessing/programming/barriers/NBarrier.java
+++ src/main/java/bit/multiprocessing/programming/barriers/NBarrier.java
@@ -2,35 +2,51 @@ package bit.multiprocessing.programming.barriers;
 
 import bit.multiprocessing.programming.ThreadUtils;
 
+import java.util.concurrent.atomic.AtomicIntegerArray;
+
 public class NBarrier implements Barrier{
 
-    private final int[] arr;
+    private final AtomicIntegerArray arr;
 
     public NBarrier(int num){
-        arr = new int[num];
-    }
+        arr = new AtomicIntegerArray(num);
 
+    }
     @Override
     public void barrier() {
         int number = ThreadUtils.getNumber();
-        if (number == 0){
-            arr[number] = 1;
-            while (arr[number+1]!=2){
-                Thread.onSpinWait();
-            }
-        }else if(number==arr.length-1) {
-            while (arr[number-1]==0){
-                Thread.onSpinWait();
-            }
-            arr[number]=2;
+        final int l = arr.length()-1;
+        if (number==0){
+            do0();
+        }else if (number==arr.length()-1){
+            doN1();
         }else {
-            while (arr[number-1]==0){
-                Thread.onSpinWait();
-            }
-            arr[number]=1;
-            while (arr[number+1]!=2){
-                Thread.onSpinWait();
-            }
+            doI(number);
         }
     }
+
+    private void do0(){
+        arr.set(0 , 1);
+        while (arr.get(1)!=2){
+            Thread.onSpinWait();
+        }
+    }
+
+    private void doN1(){
+        while (arr.get(arr.length()-1)==0){
+            Thread.onSpinWait();
+        }
+        arr.set(arr.length()-1,2);
+    }
+
+    private void doI(final int i){
+        while (arr.get(i-1)==0){
+            Thread.onSpinWait();
+        }
+        arr.set(i,1);
+        while (arr.get(i+1)!=2){
+            Thread.onSpinWait();
+        }
+    }
+
 }
